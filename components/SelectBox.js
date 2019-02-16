a8b89e3067fc9681aa745507c0153a9360189c27
Osman Kanadikirik <ookndkrk@gmail.com>
2019-02-17 00:55:21 +0300

    SelectBox created

---
+++ components/SelectBox.js
@@ -0,0 +1,42 @@
+// Icons
+import { FaAngleDown } from 'react-icons/fa';
+
+export default class SelectBox extends React.Component {
+  
+  state={
+    open: false,
+    selected: 'Seçiniz',
+  }
+  
+  open = () => {
+    this.setState({ open: !this.state.open })
+  }
+
+  select = (option) => {
+    this.setState({ selected: option, open: false })
+    this.props.select(option);
+  }
+
+
+  render() {
+    const { open, selected } = this.state;
+    const { options } = this.props
+    return (
+      <div className="select-box" onClick={this.open}>
+        <div className="select-box-head">
+          <button>{selected}</button>
+          <FaAngleDown className="ml-3 cf-blue" />
+        </div>
+        {open && 
+          <div className="select-box-options">
+            {
+              options.map(option => {
+                return <button onClick={() => this.select(option)}> {option} </button>
+              })
+            }
+          </div>
+        }
+      </div>
+    )
+  }
+}
