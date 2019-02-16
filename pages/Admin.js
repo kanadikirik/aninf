31951c26d77e9651dc8885e21d234951ec700bfa
Osman Kanadikirik <ookndkrk@gmail.com>
2019-02-16 03:11:35 +0300

    added admin elements

--- pages/Admin.js
+++ pages/Admin.js
@@ -1,17 +1,23 @@
 // Components
-import Navbar from '../components/Navbar';
+import AdminHeader from '../components/headers/AdminHeader';
 import AdminKnowledges from '../components/AdminKnowledges';
+import AdminUsers from '../components/AdminUsers';
+import AdminReports from '../components/AdminReports';
+// Styles
+import '../static/css/admin.scss';
 
 export default class Admin extends React.Component {
-
-
 
   render() {
     const { user } = this.props;
     return (
-      <div>
-        <Navbar user={user} />
-        <AdminKnowledges user={user} />
+      <div className="admin">
+        <AdminHeader user={user}/>
+        <div className="admin-content">
+          <AdminKnowledges user={user} />
+          <AdminUsers />
+          <AdminReports />
+        </div>
       </div>
     )
   }
