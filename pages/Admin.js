81329341bc99562e498cd64cc6a345ece0747482
Osman Kanadikirik <ookndkrk@gmail.com>
2019-02-24 01:55:53 +0300

    meta tags for no index created and redirections for non admin users created

--- pages/Admin.js
+++ pages/Admin.js
@@ -1,3 +1,6 @@
+import Head from 'next/head'
+// Services
+import { User } from '../services/User';
 // Components
 import AdminHeader     from '../components/headers/AdminHeader';
 import AdminKnowledges from '../components/AdminKnowledges';
@@ -8,17 +11,38 @@ import '../static/css/admin.scss';
 
 export default class Admin extends React.Component {
 
+  componentDidMount = () => {
+    User.checkCurrent(user => {
+      if(!user){
+        window.location.href = '/'
+      } else if(user.data().type !== 0) {
+        window.location.href = '/'      
+      }
+    })
+  }
+  
+
   render() {
-    const { user } = this.props;
-    return (
-      <div className="admin">
-        <AdminHeader user={user}/>
-        <div className="admin-content">
-          <AdminKnowledges user={user} />
-          <AdminUsers />
-          <AdminReports />
+    const { user, signOut } = this.props;
+    if(user && user.data().type === 0){
+      this.element = 
+        <div className="admin">
+          <Head>
+            <meta name="robots" content="noindex" />
+            <title>Admin | aninf</title>
+          </Head>
+          <AdminHeader user={user} signOut={signOut}/>
+          <div className="admin-content">
+            <AdminKnowledges user={user} />
+            <AdminUsers />
+            <AdminReports />
+          </div>
         </div>
-      </div>
+    } else {
+      return null;
+    }
+    return (
+      this.element
     )
   }
 }
