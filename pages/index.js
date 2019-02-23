3bd1770f2f1d80a6ad59f5f3ea03272b8a1de57b
Osman Kanadikirik <ookndkrk@gmail.com>
2019-02-24 01:52:26 +0300

    created meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,36 @@
+import Head from 'next/head'
 // Components
-import Navbar     from '../components/Navbar';
-import HomeHeader from '../components/headers/HomeHeader'
-import CreateKnowledge from '../components/CreateKnowledge';
-import KnowledgeList from '../components/KnowledgeList'
+import { LoadingPage } from '../components/LoadinPage';
+import Navbar          from '../components/Navbar';;
+import HomeHeader      from '../components/headers/HomeHeader';
+import KnowledgeList   from '../components/KnowledgeList';
 
 export default class index extends React.Component {
   
   render() {
-    const { loaded, user, signIn } = this.props;
+    const { loaded, user, signIn, signOut } = this.props;
     return (
-      <div className="container">
-        <Navbar user={user} />
-        <HomeHeader loaded={loaded} user={user} signIn={signIn} />
-        <KnowledgeList user={user} />
+      <div className="home">
+        <Head>
+          <title>aninf | Öğrendiklerini paylaş, paylaştıkça daha çok öğren</title>
+          <meta charSet="UTF-8" />
+          <meta name="description" content="Öğrendiklerini paylaş, paylaştıkça daha çok öğren." />
+          <meta name="keywords" content="Yazılım, yazılım paylaşımları, yazılım notları, yazılım bilgileri, yazılım dersleri" />
+          <meta name="author" content="John Doe" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        {
+          loaded ? 
+          <div className="container">
+            <Navbar user={user} signOut={signOut} />
+            <HomeHeader loaded={loaded} user={user} signIn={signIn} />
+            <KnowledgeList user={user} signIn={signIn} />
+          </div>
+          :
+          <LoadingPage />
+        }
       </div>
+
     )
   }
 }
