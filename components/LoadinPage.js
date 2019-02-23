c2556e5026758ace04b2717a15b02de99e3ae54c
Osman Kanadikirik <ookndkrk@gmail.com>
2019-02-24 01:51:02 +0300

    loading page created

---
+++ components/LoadinPage.js
@@ -0,0 +1,8 @@
+export const LoadingPage = () => {
+  return(
+    <div className="loading-page">
+      <img src="/static/img/aninf-logo.svg" />   
+      <h1 className="cf-blue">aninf</h1>
+    </div>
+  )
+}
